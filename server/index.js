8850cf121638c745a321ed52e7a57fdbbde82fc8
sefrota <miguel265>
2019-02-01 22:08:00 +0000

    Changed server index.js to debug code

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ app.get('/values/current', async(req, res) => {
   });
 });
 
-app.post('/values', async(req, res) => {
+app.post('/api/values', async(req, res) => {
   const index = req.body.index;
 
   if (parseInt(index) > 40){
